be31705d105401fe81a6ce888e4141581988aff4
Aimer Neige <aimer@linux.com>
2026-06-17 12:44:41 +0000

    bocchi

--- web/build_single_html.sh
+++ web/build_single_html.sh
@@ -5,7 +5,7 @@ WEB_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$WEB_DIR"
 
 if [ "$#" -eq 0 ]; then
-  npm run build:single -- --series lovelive,lovelive-sunshine --out web/dist/lovelive-engine.single.html
+  npm run build:single -- --series lovelive,lovelive-sunshine,bocchi-the-rock --out web/dist/lovelive-engine.single.html
 else
   npm run build:single -- "$@"
 fi
