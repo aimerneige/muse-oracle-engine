d4191d8f5cd5b0db13fb7b1ab24609db26940e45
Aimer Neige <aimer@linux.com>
2026-06-19 21:02:03 +0000

    fix(web): rebuild complete dist directory

---
+++ web/build_dist.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+set -eu
+
+WEB_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+cd "$WEB_DIR"
+
+rm -rf "$WEB_DIR/dist"
+
+if [ "$#" -eq 0 ]; then
+  npm run build:single -- --series lovelive,lovelive-sunshine,bocchi-the-rock --external-favicons --out web/dist/index.html
+else
+  npm run build:single -- "$@" --external-favicons --out web/dist/index.html
+fi
+
+cp \
+  favicon.ico \
+  favicon-16x16.png \
+  favicon-32x32.png \
+  apple-touch-icon.png \
+  android-chrome-192x192.png \
+  android-chrome-512x512.png \
+  site.webmanifest \
+  dist/
