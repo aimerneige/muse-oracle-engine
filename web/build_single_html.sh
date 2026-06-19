a32a9d8c372b0590f4cc2134be5d32657183002a
Aimer Neige <aimer@linux.com>
2026-06-19 20:53:38 +0000

    fix(web): emit only bundled index page

--- web/build_single_html.sh
+++ web/build_single_html.sh
@@ -5,17 +5,7 @@ WEB_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$WEB_DIR"
 
 if [ "$#" -eq 0 ]; then
-  npm run build:single -- --series lovelive,lovelive-sunshine,bocchi-the-rock --external-favicons --out web/dist/index.html
+  npm run build:single -- --series lovelive,lovelive-sunshine,bocchi-the-rock --out web/dist/index.html
 else
-  npm run build:single -- "$@" --external-favicons --out web/dist/index.html
+  npm run build:single -- "$@" --out web/dist/index.html
 fi
-
-cp \
-  favicon.ico \
-  favicon-16x16.png \
-  favicon-32x32.png \
-  apple-touch-icon.png \
-  android-chrome-192x192.png \
-  android-chrome-512x512.png \
-  site.webmanifest \
-  dist/
