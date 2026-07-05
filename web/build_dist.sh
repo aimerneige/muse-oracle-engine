c0a256c1ba94598e710e628e1b88c026e6405870
Aimer Neige <aimer@linux.com>
2026-07-05 13:11:38 +0000

    update script

--- web/build_dist.sh
+++ web/build_dist.sh
@@ -1,12 +1,18 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -eu
 
 WEB_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 ROOT_DIR=$(CDPATH= cd -- "$WEB_DIR/.." && pwd)
 
+SERIES=(
+  lovelive
+  lovelive-sunshine
+  bocchi-the-rock
+)
+
 cd "$ROOT_DIR"
 if [ "$#" -eq 0 ]; then
-  go run ./web/tools/export_static_data.go --series lovelive,lovelive-sunshine,bocchi-the-rock --out web/src/generated/data.js
+  go run ./web/tools/export_static_data.go --series "$(IFS=,; echo "${SERIES[*]}")" --out web/src/generated/data.js
 else
   go run ./web/tools/export_static_data.go --out web/src/generated/data.js "$@"
 fi
